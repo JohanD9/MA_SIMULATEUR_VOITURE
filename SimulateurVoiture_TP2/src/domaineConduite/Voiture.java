7216e9837607595ff5b672e5390f6f460162a16d
Debat Johan <johan.debat@gmail.com>
2014-04-09 10:33:02 +0200

    Ajout du projet

---
+++ SimulateurVoiture_TP2/src/domaineConduite/Voiture.java
@@ -0,0 +1,45 @@
+package domaineConduite;
+
+import java.util.Observable;
+
+public class Voiture extends Observable {
+
+	private int coordXEnMetres;
+	private int vitesseMetreParSecondes;
+
+	public static final int largeurDomaine = 1000;
+
+	public Voiture(int coordXEnMetres) {
+		this.coordXEnMetres = coordXEnMetres;
+		this.vitesseMetreParSecondes = 0;
+	}
+
+	public Voiture(int coordXEnMetres, int vitesseMetreParSecondes) {
+		this.coordXEnMetres = coordXEnMetres;
+		this.vitesseMetreParSecondes = vitesseMetreParSecondes;
+	}
+
+	public int getCoordXEnMetres() {
+		return coordXEnMetres;
+	}
+
+	public int getVitesseMetreParSecondes() {
+		return vitesseMetreParSecondes;
+	}
+
+	public void avancerEnFonctionDeLaVitesse() {
+		if((coordXEnMetres + vitesseMetreParSecondes + 80) > largeurDomaine){
+			coordXEnMetres = largeurDomaine - 80;
+		}
+		else{
+			coordXEnMetres += vitesseMetreParSecondes;
+		}
+		
+		notificationObservateurs();
+	}
+
+	private void notificationObservateurs() {
+		this.setChanged();
+		this.notifyObservers();
+	}
+}
